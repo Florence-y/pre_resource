ea7eea4d4ed3ce6b30fdd815011b5dedc268ea9e
Florence_y <2213576511@qq.com>
2021-10-26 20:14:11 +0800

    update

--- src/network/request.js
+++ src/network/request.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 import baseurl from '../common/const'
 
-export const status = 'build';
-
 export function request(config) {
   const instance = axios.create({
     baseURL:baseurl,
