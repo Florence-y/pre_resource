fb8b24fd2d60cc4680eeaf689f6f80320453ba54
wuyanzhen <florence590@163.com>
2021-10-27 15:41:00 +0800

    添加主页vue

---
+++ src/views/home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="home"></div>
+</template>
+
+<script>
+import { test } from "../../network/home";
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      result: null,
+    };
+  },
+  created() {
+    test().then((res) => {
+      console.log(res);
+      this.result = res;
+    });
+  },
+};
+</script>
+
+<style>
+</style>
