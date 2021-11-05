b75c064d035f7171a933c32e49ee4649a116707f
wuyanzhen <florence590@163.com>
2021-11-05 15:39:28 +0800

    解决动态v-for赋值src的bug

--- src/views/subpage/UsersMange.vue
+++ src/views/subpage/UsersMange.vue
@@ -26,10 +26,7 @@
             <div class="flex items-center space-x-3">
               <div class="avatar">
                 <div class="w-12 h-12 mask mask-squircle">
-                  <img
-                    src="/tailwind-css-component-profile-4@56w.png"
-                    alt="Avatar Tailwind CSS Component"
-                  />
+                  <img :src="require('../../assets/avtar/'+user.url)" alt="Avatar Tailwind CSS Component" />
                 </div>
               </div>
               <div>
@@ -57,7 +54,7 @@
               v-if="user.role == 1"
               class="select select-bordered w-full max-w-xs"
             >
-              <option >浏览者</option>
+              <option>浏览者</option>
               <option selected="selected">资源管理者</option>
               <option>最高管理者</option>
             </select>
@@ -65,7 +62,7 @@
               v-if="user.role == 2"
               class="select select-bordered w-full max-w-xs"
             >
-              <option >浏览者</option>
+              <option>浏览者</option>
               <option>资源管理者</option>
               <option selected="selected">最高管理者</option>
             </select>
@@ -74,7 +71,7 @@
       </tbody>
     </table>
   </div>
-    <!-- <element-upload></element-upload> -->
+  <!-- <element-upload></element-upload> -->
 </template>
 
 <script>
@@ -82,15 +79,35 @@ import NavBar from "../nav/NavBar.vue";
 // import ElementUpload from "../../components/common/Element-Upload.vue"
 export default {
   name: "UsersManage",
-  components: { NavBar},
+  components: { NavBar },
   data() {
     return {
       users: [
-        { name: "吴彦臻", job: "教务处", location: "北京", role: 0 },
-        { name: "吴晓吟", job: "财务处", location: "上海", role: 1 },
-        { name: "吴小文", job: "党建办", location: "广州", role: 2 },
+        {
+          name: "吴彦臻",
+          job: "教务处",
+          location: "北京",
+          role: 0,
+          url: "1.jpg",
+        },
+        {
+          name: "吴晓吟",
+          job: "财务处",
+          location: "上海",
+          role: 1,
+          url: "2.jpg",
+        },
+        {
+          name: "吴小文",
+          job: "党建办",
+          location: "广州",
+          role: 2,
+          url: "3.jpg",
+        },
       ],
     };
+  },
+  methods: {
   },
 };
 </script>
