f79582653244309316bb4a637568cbb1fac7bc4b
Florence_y <2213576511@qq.com>
2021-11-11 21:21:37 +0800

    更新配置

--- src/views/subpage/Profie.vue
+++ src/views/subpage/Profie.vue
@@ -5,7 +5,7 @@
       <div class="py-4 artboard artboard-demo bg-base-200">
         <ul class="menu p-4 shadow-lg bg-base-100 rounded-box">
           <li class="menu-title">
-            <span> Menu Title </span>
+            <span> 我的信息 </span>
           </li>
           <li>
             <a>
@@ -28,7 +28,7 @@
                   d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
                 ></path>
               </svg>
-              Item with icon
+              个人信息
             </a>
           </li>
           <li>
@@ -46,7 +46,7 @@
                   d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"
                 ></path>
               </svg>
-              Item with icon
+              账号安全
             </a>
           </li>
           <li>
@@ -64,7 +64,7 @@
                   d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
                 ></path>
               </svg>
-              Item with icon
+              角色设置
             </a>
           </li>
         </ul>
@@ -74,9 +74,7 @@
       <div class="profieHeader">
         <div class="avatar flex space-x-1.5">
           <div class="mb-8 rounded-btn w-24 h-24">
-            <img
-              src="http://daisyui.com/tailwind-css-component-profile-1@94w.png"
-            />
+            <img :src="avatar" />
           </div>
           <button class="btn">上传头像</button>
         </div>
@@ -122,7 +120,8 @@
             />
           </label>
         </div>
-        <div class="manOrWoman p-6 card bordered w-1/4 flex-row space-x-4">-
+        <div class="manOrWoman p-6 card bordered w-1/4 flex-row space-x-4">
+          -
           <div class="form-control">
             <label class="cursor-pointer label">
               <span class="label-text">男</span>
@@ -155,10 +154,14 @@
 
 <script>
 import NavBar from "../nav/NavBar.vue";
-
 export default {
   name: "Profie",
   components: { NavBar },
+  data() {
+    return {
+      avatar: this.$imgBaseUrl + this.$userInf.avatar,
+    };
+  },
 };
 </script>
 
