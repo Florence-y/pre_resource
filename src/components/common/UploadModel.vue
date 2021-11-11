03e80a828b15a206882b6c227cf8b05031752a69
wuyanzhen <florence590@163.com>
2021-11-11 09:33:51 +0800

    将资源申请组件化

---
+++ src/components/common/UploadModel.vue
@@ -0,0 +1,48 @@
+<template>
+  <label for="my-modal-2" class="btn btn-primary modal-button">申请</label>
+  <input type="checkbox" id="my-modal-2" class="modal-toggle" />
+  <div class="modal">
+    <div class="modal-box">
+      <!-- 数量 -->
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text">数量</span>
+        </label>
+        <input
+          type="text"
+          placeholder="请输入数量"
+          class="input input-bordered"
+        />
+      </div>
+      <!--证明说明-->
+      <div class="form-control">
+        <label class="label">
+          <span class="label-text">资源申请原因与去处</span>
+        </label>
+        <textarea
+          class="textarea h-24 textarea-bordered"
+          placeholder="请输入资源申请原因与去处"
+        ></textarea>
+      </div>
+      <!-- 文件上传 -->
+      <upload></upload>
+      <div class="modal-action">
+        <label for="my-modal-2" class="btn btn-primary">确定</label>
+        <label for="my-modal-2" class="btn">取消</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Upload from "../../components/common/Upload.vue";
+export default {
+    name:"UploadModel",
+    components:{
+        Upload
+    }
+};
+</script>
+
+<style>
+</style>
--- src/views/home/Content.vue
+++ src/views/home/Content.vue
@@ -9,41 +9,7 @@
           <h2 class="card-title">{{ item.name }}</h2>
           <p>{{ item.description }}</p>
           <div class="card-actions">
-            <label for="my-modal-2" class="btn btn-primary modal-button"
-              >申请</label
-            >
-            <input type="checkbox" id="my-modal-2" class="modal-toggle" />
-            <div class="modal">
-              <div class="modal-box">
-                <!-- 数量 -->
-                <div class="form-control">
-                  <label class="label">
-                    <span class="label-text">数量</span>
-                  </label>
-                  <input
-                    type="text"
-                    placeholder="请输入数量"
-                    class="input input-bordered"
-                  />
-                </div>
-                <!--证明说明-->
-                <div class="form-control">
-                  <label class="label">
-                    <span class="label-text">资源申请原因与去处</span>
-                  </label>
-                  <textarea
-                    class="textarea h-24 textarea-bordered"
-                    placeholder="请输入资源申请原因与去处"
-                  ></textarea>
-                </div>
-                <!-- 文件上传 -->
-                <upload></upload>
-                <div class="modal-action">
-                  <label for="my-modal-2" class="btn btn-primary">确定</label>
-                  <label for="my-modal-2" class="btn">取消</label>
-                </div>
-              </div>
-            </div>
+            <upload-model></upload-model>
             <button class="btn btn-ghost">详细信息</button>
           </div>
         </div>
@@ -54,7 +20,7 @@
 
 <script>
 import { loadResourceList } from "../../network/home";
-import Upload from "../../components/common/Upload.vue"
+import UploadModel from "../../components/common/UploadModel.vue";
 export default {
   name: "Content",
   data() {
@@ -79,7 +45,7 @@ export default {
     this.load();
   },
 
-    components: {Upload},
+  components: { UploadModel },
 };
 </script>
 
