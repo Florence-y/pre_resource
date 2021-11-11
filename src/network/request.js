83ab8fca3e134520b5307499d16adb0a263dbbcf
wuyanzhen <florence590@163.com>
2021-11-11 14:39:46 +0800

    优化代码结构

--- src/network/request.js
+++ src/network/request.js
@@ -1,18 +1,9 @@
 import axios from "axios";
 axios.defaults.withCredentials = true
 export function request(config) {
-  let instance;
-  if (config.headers !== null) {
-    instance = axios.create({
-          baseURL: "http://localhost:8080",
-      timeout: 5000,
-        headers: config.headers
-      })
-  } else {
-    instance = axios.create({
-      baseURL: "http://localhost:8080",
-      timeout: 5000
-    })
-  }
+  const instance = axios.create({
+        baseURL: "http://localhost:8080",
+        timeout: 5000,
+  });
   return instance(config);
 }
